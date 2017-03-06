605a674959833715875266c9f4c15c3f24b5847e
brent <brent726@gmail.com>
2017-03-06 20:13:14 +0800

    3/6/17

--- vic car detection/OpenCVProject/OpenCVProject.cpp
+++ vic car detection/OpenCVProject/OpenCVProject.cpp
@@ -109,7 +109,8 @@ void searchForVehicle(Mat thresholdImage, Mat &cameraFeed){
 					cout << " Height: " << height;
 					printf("\n");
 
-					if(width>50 && width <140 && height>30 && height<50)
+					//if(width>50 && width <140 && height>30 && height<50)
+					if(width>50 &&width<150 && height>30&&height<50)
 					{
 					Vehicle car;
 					
@@ -136,141 +137,190 @@ int main(int argc, char** argv) {
   // allocate the images and other structures we will need for
   // results.
   //
-  Mat imgA = imread( "stock3.png");
-  Mat imgAOrig=imgA.clone();
+  //Mat imgA = imread( "newStock2.png");
+  //Mat imgAOrig=imgA.clone();
   
-  Mat imgB = imread( "stock4.png");
-  
-  Size img_sz = imgA.size();
+  //Mat imgB = imread( "newStock1.png");
+  bool pause=false;
+ 
+  Mat curFrame, prevFrame,prevResultFrame, curResultFrame;
+  VideoCapture cap("C:\\Users\\PCBLAB_01\\Desktop\\sampleVideo.avi");
+  double fps = cap.get(CV_CAP_PROP_FPS);
+  int framepos;
+  Size img_sz;
+  Mat frameCurOrig;
+  while(true)
+  {
+		  cap >> curFrame;
+		  frameCurOrig=curFrame.clone();
+		  framepos=(int)cap.get(CV_CAP_PROP_POS_FRAMES);
+		  curResultFrame = curFrame.clone();
+		  cvtColor(curFrame, curFrame, COLOR_BGR2GRAY); 
+		 // imshow("current frame",curFrame);
+		  if(framepos!=1)
+		  {
+				 //imshow("prev frame",prevFrame);
+				  img_sz = curFrame.size();
+				  int win_size = 10;
+				  prevResultFrame = prevFrame.clone();
+				  //curResultFrame = curFrame.clone();
+				  //cvtColor(prevFrame, prevFrame, COLOR_BGR2GRAY); 
+				  //cvtColor(curFrame, curFrame, COLOR_BGR2GRAY); 
+				  //imshow("cur frame",curFrame);
+				  //imshow("prev frame",prevFrame);
+				  
+				  // The first thing we need to do is get the features
+				  // we want to track.
+				  //
+				  vector<Point2f > cornersA, cornersB;
+				  const int MAX_CORNERS = 500;
+				  cv::goodFeaturesToTrack(
+					curFrame,                         // Image to track
+					cornersA,                     // Vector of detected corners (output)
+					MAX_CORNERS,                  // Keep up to this many corners
+					0.01,                         // Quality level (percent of maximum)
+					5,                            // Min distance between corners
+					noArray(),                // Mask
+					3,                            // Block size
+					false,                        // true: Harris, false: Shi-Tomasi
+					0.04                          // method specific parameter
+				  );
 
-  int win_size = 10;
-  Mat imgResultA = imgA.clone();
-  Mat imgResultB= imgB.clone();
-  cvtColor(imgA, imgA, COLOR_BGR2GRAY); 
-  cvtColor(imgB, imgB, COLOR_BGR2GRAY); 
-  // The first thing we need to do is get the features
-  // we want to track.
-  //
-  vector<Point2f > cornersA, cornersB;
-  const int MAX_CORNERS = 500;
-  cv::goodFeaturesToTrack(
-    imgA,                         // Image to track
-    cornersA,                     // Vector of detected corners (output)
-    MAX_CORNERS,                  // Keep up to this many corners
-    0.01,                         // Quality level (percent of maximum)
-    5,                            // Min distance between corners
-    noArray(),                // Mask
-    3,                            // Block size
-    false,                        // true: Harris, false: Shi-Tomasi
-    0.04                          // method specific parameter
-  );
+				  cornerSubPix(
+					curFrame,                         // Input image
+					cornersA,                     // Vector of corners (input and output)
+					Size(win_size, win_size), // Half side length of search window
+					Size(-1,-1),              // Half side length of dead zone (-1=none)
+					TermCriteria(
+					  TermCriteria::MAX_ITER | cv::TermCriteria::EPS,
+					  20,                         // Maximum number of iterations
+					  0.03                        // Minimum change per iteration
+					)
+				  );
 
-  cornerSubPix(
-    imgA,                         // Input image
-    cornersA,                     // Vector of corners (input and output)
-    Size(win_size, win_size), // Half side length of search window
-    Size(-1,-1),              // Half side length of dead zone (-1=none)
-    TermCriteria(
-      TermCriteria::MAX_ITER | cv::TermCriteria::EPS,
-      20,                         // Maximum number of iterations
-      0.03                        // Minimum change per iteration
-    )
-  );
+				  // Call the Lucas Kanade algorithm
+				  //
+				  vector<uchar> features_found;
+				  cv::calcOpticalFlowPyrLK(
+					prevFrame,                         // Previous image
+					curFrame,                         // Next image
+					cornersA,                     // Previous set of corners (from imgA)
+					cornersB,                     // Next set of corners (from imgB)
+					features_found,               // Output vector, each is 1 for tracked
+					cv::noArray(),                // Output vector, lists errors (optional)
+					cv::Size( win_size*2+1, win_size*2+1 ), // Search window size
+					5,                            // Maximum pyramid level to construct
+					cv::TermCriteria(
+					  cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS,
+					  20,                         // Maximum number of iterations
+					  0.3                         // Minimum change per iteration
+					)
+				  );
 
-  // Call the Lucas Kanade algorithm
-  //
-  vector<uchar> features_found;
-  cv::calcOpticalFlowPyrLK(
-    imgA,                         // Previous image
-    imgB,                         // Next image
-    cornersA,                     // Previous set of corners (from imgA)
-    cornersB,                     // Next set of corners (from imgB)
-    features_found,               // Output vector, each is 1 for tracked
-    cv::noArray(),                // Output vector, lists errors (optional)
-    cv::Size( win_size*2+1, win_size*2+1 ), // Search window size
-    5,                            // Maximum pyramid level to construct
-    cv::TermCriteria(
-      cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS,
-      20,                         // Maximum number of iterations
-      0.3                         // Minimum change per iteration
-    )
-  );
+				  Mat temp=frameCurOrig.clone();
+				  Mat binCurFrame(curFrame.rows, curFrame.cols, CV_8UC1, Scalar(0));
+				  // Now make some image of what we are looking at:
+				  // Note that if you want to track cornersB further, i.e.
+				  // pass them as input to the next calcOpticalFlowPyrLK,
+				  // you would need to "compress" the vector, i.e., exclude points for which
+				  // features_found[i] == false.
+				  for( int i = 0; i < (int)cornersA.size(); i++ ) {
+					if( !features_found[i] )
+					  continue;
+					line(
+					  //imgResultA,                        // Draw onto this image
+					  binCurFrame,
+					  cornersA[i],                 // Starting here
+					  cornersB[i],                 // Ending here
+					  cv::Scalar(255),        // This color
+					  2,                           // This many pixels wide
+					  CV_AA                  // Draw line in this style
+					  );
+					line(
+					  //prevFrame,                        // Draw onto this image
+					  temp,
+					  cornersA[i],                 // Starting here
+					  cornersB[i],                 // Ending here
+					  cv::Scalar(0,255,0),        // This color
+					  2,                           // This many pixels wide
+					  CV_AA                  // Draw line in this style
+					  );
+				  }
+				  cv::imshow( "LK Optical Flow result A", temp);
+					//sobel parameters
+					Mat grad;
+					Mat grad_x, grad_y;
+					Mat abs_grad_x, abs_grad_y;
+					int scale = 1;
+					int delta = 0;
+					int ddepth = CV_16S;
+					Mat thresholdImage;
 
+					GaussianBlur(curFrame, curFrame, Size(15,15), 0, 0, BORDER_DEFAULT );
+					//sobel 
+					Sobel( curFrame, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+					convertScaleAbs( grad_x, abs_grad_x );
+					Sobel( curFrame, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+					convertScaleAbs( grad_y, abs_grad_y );
+					addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+					//cv::imshow("Sobel Image", grad);
+					cv::threshold(grad,thresholdImage,25,255,THRESH_BINARY);
+					//cv::imshow("Threshold Image", thresholdImage);
+					//morphologyEx(thresholdImage,thresholdImage,MORPH_OPEN,Mat::ones(3,3,CV_8SC1),Point(1,1),2);
+					//cv::imshow("Morphed Image", thresholdImage);
+					curResultFrame=thresholdImage+binCurFrame;
 
-  Mat binImageResultA(imgA.rows, imgA.cols, CV_8UC1, Scalar(0));
-  // Now make some image of what we are looking at:
-  // Note that if you want to track cornersB further, i.e.
-  // pass them as input to the next calcOpticalFlowPyrLK,
-  // you would need to "compress" the vector, i.e., exclude points for which
-  // features_found[i] == false.
-  for( int i = 0; i < (int)cornersA.size(); i++ ) {
-    if( !features_found[i] )
-      continue;
-    line(
-      //imgResultA,                        // Draw onto this image
-	  binImageResultA,
-	  cornersA[i],                 // Starting here
-      cornersB[i],                 // Ending here
-      cv::Scalar(255),        // This color
-	  2,                           // This many pixels wide
-      CV_AA                  // Draw line in this style
-      );
-	line(
-      imgResultB,                        // Draw onto this image
-      cornersA[i],                 // Starting here
-      cornersB[i],                 // Ending here
-      cv::Scalar(0,255,0),        // This color
-	  2,                           // This many pixels wide
-      CV_AA                  // Draw line in this style
-      );
+					  //cv::imshow( "ImageA", curFrame );
+					  //cv::imshow( "ImageB", prevFrame );
+					  //cv::imshow( "Combined LK Optical Flow + Sobel result A", curResultFrame );
+					 // cv::imshow( "LK Optical Flow result B", imgResult);
+
+					  Mat frame1=frameCurOrig.clone();
+					  Mat threshCpy=thresholdImage.clone();
+					  if(true)
+					   {
+
+						searchForVehicle(threshCpy,frame1);
+					   }
+					  imshow("sobel",frame1);
+					  frame1=frameCurOrig.clone();
+					  if(true)
+					  {
+						searchForVehicle(curResultFrame,frame1);
+					  }
+					  imshow("output lk+sobel",frame1);
+		      }
+		 
+			  
+			  prevFrame=curFrame;
+			  //waitKey();
+			  pause=false;
+			  switch(waitKey(100/fps)){
+				case 27: //'esc' key has been pressed, exit program.
+				return 0;
+				case 112: //'p' has been pressed. this will pause/resume the code.
+				pause = !pause;
+				if(pause == true){ cout<<"Code paused, press 'p' again to resume"<<endl;
+				while (pause == true){
+					//stay in this loop until 
+					switch (waitKey()){
+						//a switch statement inside a switch statement? Mind blown.
+					case 112: 
+						//change pause back to false
+						pause = false;
+						cout<<"Code Resumed"<<endl;
+						break;
+					case 115:
+						//screen capture
+						//imwrite( "E:/capturedimage1.jpg", frame1 );
+						cout<<"Capture Image"<<endl;
+						break;
+						}
+				  }
+				}
+			  }
+			  //cv::waitKey(0);
   }
-  cv::imshow( "LK Optical Flow result A", imgResultA);
-	//sobel parameters
-	Mat grad;
-	Mat grad_x, grad_y;
-	Mat abs_grad_x, abs_grad_y;
-	int scale = 1;
-	int delta = 0;
-	int ddepth = CV_16S;
-	Mat thresholdImage;
-
-    GaussianBlur(imgA, imgA, Size(13,13), 0, 0, BORDER_DEFAULT );
-	GaussianBlur(imgB, imgB, Size(13,13), 0, 0, BORDER_DEFAULT );
-	//sobel 
-	Sobel( imgA, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-	convertScaleAbs( grad_x, abs_grad_x );
-	Sobel( imgA, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-	convertScaleAbs( grad_y, abs_grad_y );
-	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
-	cv::imshow("Sobel Image", grad);
-	cv::threshold(grad,thresholdImage,25,255,THRESH_BINARY);
-	cv::imshow("Threshold Image", thresholdImage);
-	//morphologyEx(thresholdImage,thresholdImage,MORPH_OPEN,Mat::ones(3,3,CV_8SC1),Point(1,1),2);
-	//cv::imshow("Morphed Image", thresholdImage);
-	imgResultA=thresholdImage+binImageResultA;
-
-	  cv::imshow( "ImageA", imgA );
-	  cv::imshow( "ImageB", imgB );
-	  cv::imshow( "Combined LK Optical Flow + Sobel result A", imgResultA );
-	  cv::imshow( "LK Optical Flow result B", imgResultB );
-
-	  Mat frame1=imgAOrig.clone();
-	  Mat threshCpy=thresholdImage.clone();
-	  if(true)
-	   {
-
-		searchForVehicle(threshCpy,frame1);
-	   }
-	  imshow("sobel",frame1);
-	  frame1=imgAOrig;
-	  if(true)
-	   {
-
-		searchForVehicle(imgResultA,frame1);
-	  }
-	  imshow("output lk+sobel",frame1);
-	  cv::waitKey(0);
-
   return 0;
 }
 
